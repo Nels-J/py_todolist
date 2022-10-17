40049d1dbab73ddbee66b3d9441721e66ace504d
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-17 14:11:41 +0200

    menu: dispatching answers

--- todolist.py
+++ todolist.py
@@ -2,7 +2,7 @@ todo = []
 done = []
 #menu
 print("Bienvenue sur votre todolist. Que souhaitez vous faire?")
-userInput = input(""
+userInput = input(
                   "add => ajouter une tâche \n"
                   "done => marquer une tâche comme finie \n"
                   "update => modifier une tâche\n"
@@ -11,3 +11,28 @@ userInput = input(""
                   "list-all => lister toutes les tâches\n"
                   "quit => quitter l'application"
 )
+while userInput != "quit":
+    if userInput == "add":
+       print("add")
+    elif userInput == "done":
+       print("done")
+    elif userInput == "update":
+       print("update")
+    elif userInput == "list":
+       print("list")
+    elif userInput == "list-done":
+       print("list-done")
+    elif userInput == "list-all":
+       print("list-all")
+    elif userInput == "quit":
+       print("quit")
+    else:
+       print("Invalid answer")
+    userInput = input(
+        "add => ajouter une tâche \n"
+        "done => marquer une tâche comme finie \n"
+        "update => modifier une tâche\n"
+        "list => lister les tâches en cours\n"
+        "list-done => lister les tâches terminées\n"
+        "list-all => lister toutes les tâches\n"
+        "quit => quitter l'application")
