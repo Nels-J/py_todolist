64b0fedca65f5a94763c9387dcc93a75b36da648
VWACOGNE <victor.wacogne@le-campus-numerique.fr>
2022-10-17 14:21:11 +0200

    affichage proposition

---
+++ main.py
@@ -0,0 +1,14 @@
+def main_menu():
+
+    print(
+        "Ajouter une tâche (add)"
+        "\nEffectuer une tâche (done)"
+        "\nModifier le libellé d'une tâche (update)"
+        "\nLister les tâches en cours (list)"
+        "\nLister les tâches terminées (list-done)"
+        "\nLister toutes les tâches (list-all)"
+        "\nQuitter (quit)")
+    print("********Que voulez-vous faire ?**********")
+
+
+main_menu()
