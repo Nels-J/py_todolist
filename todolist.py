fbd133c2472660fdba996ebb42b4a76f9d5f0a3d
Nelson JESUS <74188213+Nels-J@users.noreply.github.com>
2022-10-17 15:55:38 +0200

    init

---
+++ todolist.py
@@ -0,0 +1,44 @@
+print('\nWelcome to your To Do List application !\n')
+
+
+def print_menu():
+    print("To add a new task, use 'add' command")
+    print("To close a task, use 'done' command")
+    print("To amend task name, use 'update' command")
+    print("To list pending tasks, use 'list' command")
+    print("To list closed tasks, use 'list-done' command")
+    print("To list all tasks, use 'list-all' command")
+    print("To exit the application, use 'quit' command")
+
+
+def user_input():
+    new_command = input("\nPlease enter your command :")
+    try:
+        validate_command(new_command)
+    except Exception as err:
+        print(err)
+        print_menu()
+        user_input()
+
+
+def validate_command(new_command):
+    if new_command == "quit":
+        print("\nGoodbye\n")
+    elif new_command == "add":
+        raise Exception("\nCommand under development: please pick another one\n")
+    elif new_command == "done":
+        raise Exception("\nCommand under development: please pick another one\n")
+    elif new_command == "update":
+        raise Exception("\nCommand under development: please pick another one\n")
+    elif new_command == "list":
+        raise Exception("\nCommand under development: please pick another one\n")
+    elif new_command == "list-done":
+        raise Exception("\nCommand under development: please pick another one\n")
+    elif new_command == "list-all":
+        raise Exception("\nCommand under development: please pick another one\n")
+    else:
+        raise Exception('\ninvalid command, please retry\n')
+
+
+print_menu()
+user_input()
