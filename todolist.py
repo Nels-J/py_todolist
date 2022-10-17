14916e9bf92f62702e4e68c90286520432e2907f
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-17 15:43:57 +0200

    refactor: all functions called in main()

--- todolist.py
+++ todolist.py
@@ -1,17 +1,14 @@
-#menu
-print("Bienvenue sur votre todolist. Que souhaitez vous faire?")
-
-
-userInput = input(
-                  "add => ajouter une tâche \n"
-                  "done => marquer une tâche comme finie \n"
-                  "update => modifier une tâche\n"
-                  "list => lister les tâches en cours\n"
-                  "list-done => lister les tâches terminées\n"
-                  "list-all => lister toutes les tâches\n"
-                  "quit => quitter l'application"
-)
-
+def menu():
+    userInput = input(
+        "add => ajouter une tâche \n"
+        "done => marquer une tâche comme finie \n"
+        "update => modifier une tâche\n"
+        "list => lister les tâches en cours\n"
+        "list-done => lister les tâches terminées\n"
+        "list-all => lister toutes les tâches\n"
+        "quit => quitter l'application"
+    )
+    return userInput
 
 # Fonctions:
 def add_task():
@@ -41,35 +38,36 @@ def list_all_tasks():
 def quit_todo_list():
     raise ValueError()
 
+def main():
 
-#dispatch
-while userInput != "quit":
-    try:
-        if userInput == "add":
-            add_task()
-        elif userInput == "done":
-            done_task()
-        elif userInput == "update":
-            update_task()
-        elif userInput == "list":
-            list_pending_tasks()
-        elif userInput == "list-done":
-            list_done_tasks()
-        elif userInput == "list-all":
-            list_all_tasks()
-        elif userInput == "quit":
-            quit_todo_list()
-        else:
-            print("Réponse invalide")
-    except ValueError:
-            print("La fonction de l'instruction ", userInput, "n'est pas encore implémentée")
-    userInput = input(
-        "add => ajouter une tâche \n"
-        "done => marquer une tâche comme finie \n"
-        "update => modifier une tâche\n"
-        "list => lister les tâches en cours\n"
-        "list-done => lister les tâches terminées\n"
-        "list-all => lister toutes les tâches\n"
-        "quit => quitter l'application")
+    # menu
+    print("Bienvenue sur votre todolist. Que souhaitez vous faire?")
+    userInput = menu()
+
+    # dispatch
+    while userInput != "quit":
+        try:
+            if userInput == "add":
+                add_task()
+            elif userInput == "done":
+                done_task()
+            elif userInput == "update":
+                update_task()
+            elif userInput == "list":
+                list_pending_tasks()
+            elif userInput == "list-done":
+                list_done_tasks()
+            elif userInput == "list-all":
+                list_all_tasks()
+            elif userInput == "quit":
+                quit_todo_list()
+            else:
+                print("Réponse invalide")
+        except ValueError:
+            print("La fonction de l'instruction '", userInput, "' n'est pas encore implémentée")
+        userInput=menu()
+    print("À bientôt!")
+
+main()
 
 
