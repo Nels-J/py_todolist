6ba6b644b957c901d216122417015907a579151d
VWACOGNE <victor.wacogne@le-campus-numerique.fr>
2022-10-17 14:43:59 +0200

    gestion des erreurs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def main_menu():
-
     print(
         "Ajouter une tâche (add)"
         "\nEffectuer une tâche (done)"
@@ -9,6 +8,56 @@ def main_menu():
         "\nLister toutes les tâches (list-all)"
         "\nQuitter (quit)")
     print("********Que voulez-vous faire ?**********")
-    reponse = input()
+
+    response = input()
+    try:
+        if response == 'add':
+            add()
+        elif response == 'done':
+            done()
+        elif response == 'update':
+            update()
+        elif response == 'list':
+            list_not_done()
+        elif response == 'list-done':
+            list_done()
+        elif response == 'list-all':
+            list_all()
+        elif response == 'quit':
+            quit()
+        else:
+            raise Exception
+
+    except Exception as e:
+        message = "la commande n'existe pas"
+        if e.__class__ == NotImplementedError:
+            message = 'désolé vous ne pouvez pas utiliser cette fonction pour l\'instant'
+
+        print(message)
+
+
+def add():
+    raise NotImplementedError
+
+
+def done():
+    raise NotImplementedError
+
+
+def update():
+    raise NotImplementedError
+
+
+def list_not_done():
+    raise NotImplementedError
+
+
+def list_done():
+    raise NotImplementedError
+
+
+def list_all():
+    raise NotImplementedError
+
 
 main_menu()
