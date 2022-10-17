be768f2f0a8ffec6e462473d7f3dad7e196f5965
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-17 14:02:22 +0200

    menu

---
+++ todolist.py
@@ -0,0 +1,13 @@
+todo = []
+done = []
+#menu
+print("Bienvenue sur votre todolist. Que souhaitez vous faire?")
+userInput = input(""
+                  "add => ajouter une tâche \n"
+                  "done => marquer une tâche comme finie \n"
+                  "update => modifier une tâche\n"
+                  "list => lister les tâches en cours\n"
+                  "list-done => lister les tâches terminées\n"
+                  "list-all => lister toutes les tâches\n"
+                  "quit => quitter l'application"
+)
