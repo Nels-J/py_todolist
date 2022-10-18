352d14ef5b52a82cae9364ec55213efdad2bba9e
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-10-18 09:22:41 +0200

    add main method and refacto with KarineML

--- todolist.py
+++ todolist.py
@@ -1,6 +1,3 @@
-print('\nWelcome to your To Do List application !\n')
-
-
 def print_menu():
     print("To add a new task, use 'add' command")
     print("To close a task, use 'done' command")
@@ -11,62 +8,54 @@ def print_menu():
     print("To exit the application, use 'quit' command")
 
 
-def user_input():
-    new_command = input("\nPlease enter your command :")
-    try:
-        validate_command(new_command)
-    except Exception as err:
-        print(err)
-        print_menu()
-        user_input()
+def add_task():
+    raise Exception("Command under development: please pick another one")
 
 
-def quit():
-    print("\nGoodbye\n")
+def close_task():
+    raise Exception("Command under development: please pick another one")
 
 
-def add():
-    raise Exception("\nCommand under development: please pick another one\n")
+def update_task():
+    raise Exception("Command under development: please pick another one")
 
 
-def done():
-    raise Exception("\nCommand under development: please pick another one\n")
+def list_pending_tasks():
+    raise Exception("Command under development: please pick another one")
 
 
-def update():
-    raise Exception("\nCommand under development: please pick another one\n")
+def list_done_tasks():
+    raise Exception("Command under development: please pick another one")
 
 
-def list():
-    raise Exception("\nCommand under development: please pick another one\n")
+def list_all_tasks():
+    raise Exception("Command under development: please pick another one")
 
 
-def list_done():
-    raise Exception("\nCommand under development: please pick another one\n")
+def main():
+    print('\nWelcome to your To Do List application !\n')
+    print_menu()
+    user_input = input("\nPlease enter your command :")
+    while user_input != "quit":
+        try:
+            if user_input == "add":
+                add_task()
+            elif user_input == "done":
+                close_task()
+            elif user_input == "update":
+                update_task()
+            elif user_input == "list":
+                list_pending_tasks()
+            elif user_input == "list-done":
+                list_done_tasks()
+            elif user_input == "list-all":
+                list_all_tasks()
+            else:
+                raise Exception('invalid command, please retry')
+        except Exception as err:
+            print(err)
+        user_input = input("Please enter your command :")
+    print("Goodbye")
 
-
-def list_all():
-    raise Exception("\nCommand under development: please pick another one\n")
-
-
-def validate_command(new_command):
-    if new_command == "quit":
-        quit()
-    elif new_command == "add":
-        add()
-    elif new_command == "done":
-        done()
-    elif new_command == "update":
-        update()
-    elif new_command == "list":
-        list()
-    elif new_command == "list-done":
-        list_done()
-    elif new_command == "list-all":
-        list_all()
-    else:
-        raise Exception('\ninvalid command, please retry\n')
-
-
-print_menu()
-user_input()
+if __name__ == "__main__":
+    main()
