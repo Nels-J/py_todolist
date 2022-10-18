a72813712e0deedc82c07f24bc881bd602964d12
Mika Cocchi <mika.cocchi@le-campus-numerique.fr>
2022-10-18 10:39:41 +0200

    Manage a simple task in a to do list

--- main.py
+++ main.py
@@ -1,33 +1,3 @@
-def add():
-    print('VOUS AVEZ TAPE ADD')
-    raise ValueError('La fonctionnalité n\'a pas encore été implémentée !')
-
-
-def done():
-    print('VOUS AVEZ TAPE DONE')
-    raise ValueError('La fonctionnalité n\'a pas encore été implémentée !')
-
-
-def update():
-    print('VOUS AVEZ TAPE UPDATE')
-    raise ValueError('La fonctionnalité n\'a pas encore été implémentée !')
-
-
-def list():
-    print('VOUS AVEZ TAPE LIST')
-    raise ValueError('La fonctionnalité n\'a pas encore été implémentée !')
-
-
-def list_done():
-    print('VOUS AVEZ TAPE LIST DONE')
-    raise ValueError('La fonctionnalité n\'a pas encore été implémentée !')
-
-
-def list_all():
-    print('VOUS AVEZ TAPE LIST ALL')
-    raise ValueError('La fonctionnalité n\'a pas encore été implémentée !')
-
-
 def print_menu():
     print("voici le menu :")
     print("Ajouter une tâche (commande 'add')")
@@ -39,37 +9,76 @@ def print_menu():
     print("Quitter (command 'quit')")
 
 
-def user_input(user_choice):
+def add():
+    task_name = input("Quel est le nom de votre nouvelle tache ?")
+    task_status = False
+    return task_name, task_status
+
+
+def done():
+    print('VOUS AVEZ EFFECTUÉ VOTRE TÂCHE')
+    return True
+
+
+def update():
+    new_name = input('Quel est le nouveau nom de votre tâche ?')
+    return new_name
+
+
+def list_in_progress(task_name, task_status):
+    if not task_status:
+        print(task_name)
+    else:
+        print('Aucune tâche en cours')
+
+
+def list_done(task_name, task_status):
+    if task_status:
+        print(task_name)
+    else:
+        print('Aucune tâche terminée')
+
+
+def list_all(task_name):
+    print('Voicis toutes les tâches: \n' + task_name)
+
+
+def user_input(user_choice, task_name, task_status):
     try:
         if user_choice == 'add':
-            add()
+            task_name, task_status = add()
         elif user_choice == 'done':
-            done()
+            task_status = done()
         elif user_choice == 'update':
-            update()
+            task_name = update()
         elif user_choice == 'list':
-            list()
+            list_in_progress(task_name, task_status)
         elif user_choice == 'list-done':
-            list_done()
+            list_done(task_name, task_status)
         elif user_choice == 'list-all':
-            list_all()
+            list_all(task_name)
         else:
             raise ValueError('La fonctionnalité n\'existe pas !')
 
     except ValueError as e:
         print(e)
 
+    return task_name, task_status
+
 
 def main():
     want_to_quit = False
+    task_name = ""
+    task_status = False
     while not want_to_quit:
         print_menu()
         user_choice = input('Que souhaitez-vous faire ?\n')
         if user_choice == 'quit':
             want_to_quit = True
         else:
-            user_input(user_choice)
+            task_name, task_status = user_input(user_choice, task_name, task_status)
 
 
 if __name__ == '__main__':
     main()
+    print('╭∩╮(◉_◉)╭∩╮')
