2e07fcfa34ece5719962f21e19f626112257a532
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-10-20 09:21:52 +0200

    refacto and tasks arguments deleted and method done added in task class

--- todolist.py
+++ todolist.py
@@ -28,31 +28,32 @@ class TasksList:
     def __init__(self):
         self.tasks_list = []
 
-    def add_task(self, tasks, interface_display):
+    def add_task(self, interface_display):
         task_name = input("Saisir la tache : ")
         task = Task(task_name)
         interface_display.print(f'Vous venez de créer la tache: {task.name}')
         self.tasks_list.append(task)
-        self.display_all_tasks(self.tasks_list, interface_display)
         return self.tasks_list
 
-    def update_task_in_list(self, tasks, interface_display):
+    def update_task_in_list(self, interface_display):
         if not self.tasks_list:
             interface_display.print("Pas de tâche à afficher")
         else:
             index = len(self.tasks_list)
             while index >= len(self.tasks_list):
                 try:
+                    self.display_all_tasks(interface_display)
                     index = int(input("Saissez le numéro de la tâche à renommer (à partir de 0)"))
-                    if index >= len(self.tasks_list):
+                    if index >= len(self.tasks_list) or index < 0:
                         raise InvalidValueException
                 except InvalidValueException as e:
                     interface_display.print(e)
+                except Exception:
+                    interface_display.print("Merci de saisir un nombre")
             self.tasks_list[index] = self.tasks_list[index].update_task(interface_display)
-            self.display_all_tasks(self.tasks_list, interface_display)
         return self.tasks_list
 
-    def mark_task_as_done(self, tasks, interface_display):
+    def mark_task_as_done_in_list(self, interface_display):
         pending_tasks = []
         for task in self.tasks_list:
             if task.status == "A faire":
@@ -63,33 +64,31 @@ class TasksList:
             index = len(self.tasks_list)
             while index >= len(self.tasks_list):
                 try:
+                    self.display_pending_tasks(interface_display)
                     index = int(input("Saissez le numéro de la tâche à clôturer (à partir de 0)"))
-                    if index >= len(self.tasks_list):
+                    if index >= len(self.tasks_list) or index < 0:
                         raise InvalidValueException
-                except Exception:
-                    interface_display.print("Merci de saisir un nombre")
                 except InvalidValueException as e:
                     interface_display.print(e)
-            self.tasks_list[index].status = "Terminée"
-            interface_display.print(
-                f'Votre tâche: {self.tasks_list[index].name} est à présent: {self.tasks_list[index].status}')
-            self.display_all_tasks(self.tasks_list, interface_display)
+                except Exception:
+                    interface_display.print("Merci de saisir un nombre")
+            self.tasks_list[index].mark_task_as_done(interface_display)
         return self.tasks_list
 
-    def display_pending_tasks(self, tasks, interface_display):
+    def display_pending_tasks(self, interface_display):
         interface_display.print("******* Toutes vos tâches en cours: *******")
         for task in self.tasks_list:
             if task.status == "A faire":
                 interface_display.print(f"{task.name} => {task.status}")
         return self.tasks_list
 
-    def display_all_tasks(self, tasks, interface_display):
+    def display_all_tasks(self, interface_display):
         interface_display.print("******* Toutes vos tâches: *******")
-        self.display_pending_tasks(self.tasks_list, interface_display)
-        self.display_done_tasks(self.tasks_list, interface_display)
+        self.display_pending_tasks(interface_display)
+        self.display_done_tasks(interface_display)
         return self.tasks_list
 
-    def display_done_tasks(self, tasks, interface_display):
+    def display_done_tasks(self, interface_display):
         interface_display.print("******* Toutes vos tâches terminées: *******")
         for task in self.tasks_list:
             if task.status == "Terminée":
@@ -107,6 +106,11 @@ class Task:
         interface_display.print(f"Votre tâche est renommée en : {self.name}")
         return self
 
+    def mark_task_as_done(self, interface_display):
+        self.status = "Terminée"
+        interface_display.print(f"Votre tâche {self.name} est marquée comme terminée")
+        return self
+
 
 def main():
     interface_display = InterfaceDisplay()
@@ -114,7 +118,7 @@ def main():
     menu = {
         "add": ("Ajouter une tâche", "add", tasks.add_task),
         "update": ("Changer le nom d'une tâche", "update", tasks.update_task_in_list),
-        "done": ("Marquer une tâche comme terminée", "done", tasks.mark_task_as_done),
+        "done": ("Marquer une tâche comme terminée", "done", tasks.mark_task_as_done_in_list),
         "list": ("Lister les tâches en cours", "list", tasks.display_pending_tasks),
         "list_done": ("Lister les tâches terminées", "list_done", tasks.display_done_tasks),
         "list_all": ("Lister toutes les tâches", "list_all", tasks.display_all_tasks),
@@ -131,7 +135,7 @@ def main():
 def do_action(command, tasks, menu, interface_display):
     try:
         if command in menu:
-            tasks = menu[command][2](tasks, interface_display)
+            tasks = menu[command][2](interface_display)
         else:
             raise InvalidCommandException
     except InvalidCommandException as e:
